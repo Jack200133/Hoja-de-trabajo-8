94cc5dbfd354af86464a9f49bbb2b893d9b23cee
Jack200133 <car20593@uvg.edu.gt>
2021-04-21 20:01:11 -0600

    Creacion del priority Queu junto a el Vector Heap

---
+++ src/Main.java
@@ -0,0 +1,6 @@
+public class Main {
+    public static void main(String[] args) {
+        Modelo md = new Modelo();
+        md.agregar();
+    }
+}
---
+++ src/Modelo.java
@@ -0,0 +1,34 @@
+import java.io.File;
+import java.util.Scanner;
+
+public class Modelo {
+    VectorHeap vh = new VectorHeap();
+    VectorHeapSegunda vhs = new VectorHeapSegunda();
+    public void agregar(){
+        try {
+            File myObj = new File("pacientes.txt");
+            Scanner myReader = new Scanner(myObj);
+            while (myReader.hasNextLine()) {
+                String data = myReader.nextLine();
+                String[] temp = data.split(",");
+                Pacientes pt = new Pacientes(temp[2],temp[1],temp[0]);
+                vh.add(pt);
+                vhs.add(pt);
+            }
+            myReader.close();
+        }catch (Exception e) {
+            System.out.println("Archivo no encontrado");
+            e.printStackTrace();
+        }
+
+        for (int i = 0; i < vh.size();) {
+            System.out.println(vh.remove());
+        }
+
+        System.out.println("------------------------SEGUNDA IMPLEMENTACION---------------------");
+        for (int i = 0; i < vhs.size();) {
+            System.out.println(vhs.remove());
+        }
+
+    }
+}
